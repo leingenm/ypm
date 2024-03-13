dcf47acae3813226891ba9e807dfeb55daa486e6
Roman Mager <roman.mager@outlook.com>
2024-03-13 13:27:38 +0200

    Reindent code with 4 spaces

--- src/main/java/com/ypm/YoutubePlaylistManagerApplication.java
+++ src/main/java/com/ypm/YoutubePlaylistManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YoutubePlaylistManagerApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(YoutubePlaylistManagerApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubePlaylistManagerApplication.class, args);
+    }
 
 }
