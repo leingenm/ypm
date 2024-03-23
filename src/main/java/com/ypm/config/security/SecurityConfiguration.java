7cc77ada84a3963dbdeef540c6bf061697495955
David Rabko <davidryabko13@gmail.com>
2024-03-23 13:30:18 +0100

    Cover controllers and services with smoke unit tests

--- src/main/java/com/ypm/config/security/SecurityConfiguration.java
+++ src/main/java/com/ypm/config/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +17,7 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
             .oauth2Login(config -> {
                 config.defaultSuccessUrl("/auth/success", true);
-                config.failureUrl("/auth/error");
+                config.failureHandler(new SimpleUrlAuthenticationFailureHandler("/auth/error"));
             })
             .build();
     }
--- src/test/java/com/ypm/service/YouTubeServiceTest.java
+++ src/test/java/com/ypm/service/YouTubeServiceTest.java
@@ -1,7 +1,73 @@
 package com.ypm.service;
 
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
-public class YouTubeServiceTest {
+class YouTubeServiceTest {
+    private final String accessToken =
+        "ey.aphktnp.eyJzdWIiOiJ0ZXN0IiwibmFtZSI6IlRlc3QiLCJpYXQiOjE2MjMwNjMwMjJ9";
+
+    @Mock
+    private YouTube youTubeClient;
+
+    @InjectMocks
+    private YouTubeService youTubeService;
+
+    @Test
+    void givenCorrectData_whenGetPlayLists_thenPlaylistsFound() throws IOException {
+        var playlists = mock(YouTube.Playlists.class);
+        var playlistsList = mock(YouTube.Playlists.List.class);
+        var expectedPlaylist = new Playlist();
+        expectedPlaylist.setSnippet(new PlaylistSnippet().setTitle("Playlist Title"));
+        var playlistsListResponse = new PlaylistListResponse()
+            .setItems(List.of(expectedPlaylist));
+
+        when(youTubeClient.playlists()).thenReturn(playlists);
+        when(playlists.list(List.of("snippet"))).thenReturn(playlistsList);
+        when(playlistsList.setAccessToken(accessToken)).thenReturn(playlistsList);
+        when(playlistsList.setMine(true)).thenReturn(playlistsList);
+        when(playlistsList.execute()).thenReturn(playlistsListResponse);
+
+        List<Playlist> result = youTubeService.getMyPlayLists(accessToken);
+
+        assertEquals(1, result.size());
+        assertEquals(expectedPlaylist, result.get(0));
+        assertEquals("Playlist Title", result.get(0).getSnippet().getTitle());
+    }
+
+    @Test
+    void testGetPlayListVideos() throws IOException {
+        String playlistId = "asdsdta_hdfyZSDF";
+        var playlistItems = mock(YouTube.PlaylistItems.class);
+        var playlistItemsList = mock(YouTube.PlaylistItems.List.class);
+        var expectedPlaylistItem = new PlaylistItem();
+        var snippet = new PlaylistItemSnippet();
+        snippet.setTitle("Video Title");
+        expectedPlaylistItem.setSnippet(snippet);
+        var playlistItemsListResponse = new PlaylistItemListResponse()
+            .setItems(List.of(expectedPlaylistItem));
+
+        when(youTubeClient.playlistItems()).thenReturn(playlistItems);
+        when(playlistItems.list(List.of("snippet"))).thenReturn(playlistItemsList);
+        when(playlistItemsList.setAccessToken(accessToken)).thenReturn(playlistItemsList);
+        when(playlistItemsList.setPlaylistId(playlistId)).thenReturn(playlistItemsList);
+        when(playlistItemsList.execute()).thenReturn(playlistItemsListResponse);
+
+        List<VideoSnippet> result = youTubeService.getPlayListVideos(accessToken, playlistId);
+
+        assertEquals(1, result.size());
+        assertEquals("Video Title", result.get(0).getTitle());
+    }
 }
