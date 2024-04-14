8c438fb13481e2b6296d2d382b996c626f92cbf0
David Rabko <davidryabko13@gmail.com>
2024-04-14 15:29:28 +0200

    Add exception handling tests

--- src/test/java/com/ypm/controller/VideoControllerTest.java
+++ src/test/java/com/ypm/controller/VideoControllerTest.java
@@ -1,5 +1,9 @@
 package com.ypm.controller;
 
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpResponseException;
 import com.ypm.service.VideoService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +14,10 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -38,4 +43,30 @@ class VideoControllerTest {
         mockMvc.perform(delete("/videos/{videoId}", "someId").with(login))
             .andExpect(status().isNoContent());
     }
+
+    @Test
+    void givenVideoServiceThrowsGoogleJsonResponseException_whenDeleteVideos_thenThrowsException()
+        throws Exception {
+
+        var login = oauth2Login()
+            .clientRegistration(this.clientRegistrationRepository.findByRegistrationId("google"));
+
+        GoogleJsonError error = new GoogleJsonError();
+        error.setCode(400);
+        error.setMessage("Something went wrong!");
+        GoogleJsonResponseException googleJsonResponseException = new GoogleJsonResponseException(
+            new HttpResponseException.Builder(400, "Something went wrong!",
+                new HttpHeaders()), error);
+
+        doThrow(googleJsonResponseException).when(videosService).deleteVideo(any(), any());
+
+        mockMvc.perform(delete("/videos/{videoId}", "someId").with(login))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.code").value(400))
+            .andExpect(jsonPath("$.message").value("Something went wrong!"))
+            .andExpect(jsonPath("$.date").isString());
+
+        verify(videosService, times(1)).deleteVideo(any(), any());
+    }
+
 }
