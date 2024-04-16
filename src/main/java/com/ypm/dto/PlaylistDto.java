0b7378313575bffbbad8079e0da294e3abcfad4e
Roman Mager <roman.mager@outlook.com>
2024-04-16 17:30:01 +0300

    Remove unnecessary validation

--- src/main/java/com/ypm/dto/PlaylistDto.java
+++ src/main/java/com/ypm/dto/PlaylistDto.java
@@ -1,10 +1,8 @@
 package com.ypm.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
 import java.util.Optional;
 
-public record PlaylistDto(@NotBlank String title,
+public record PlaylistDto(String title,
                           Optional<String> description,
-                          @NotBlank String status) {
+                          String status) {
 }
