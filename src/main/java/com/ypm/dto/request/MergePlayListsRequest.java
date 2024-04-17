b3074a446e48427ea9865cbe65f15f34b81cd523
Roman Mager <roman.mager@outlook.com>
2024-04-17 18:10:21 +0300

    Add JsonProperty to set name for dto

--- src/main/java/com/ypm/dto/request/MergePlayListsRequest.java
+++ src/main/java/com/ypm/dto/request/MergePlayListsRequest.java
@@ -1,9 +1,10 @@
 package com.ypm.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ypm.dto.PlaylistDto;
 
 import java.util.List;
 
-public record MergePlayListsRequest(PlaylistDto playlistDto,
+public record MergePlayListsRequest(@JsonProperty(value = "playlistDetails") PlaylistDto playlistDto,
                                     List<String> playListsIds,
                                     boolean deleteAfterMerge) { }
