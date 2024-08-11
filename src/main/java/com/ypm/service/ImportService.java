73f5b6947817d08010c8bf6f270d3fcb43aea966
Roman Mager <roman.mager@outlook.com>
2024-08-11 13:52:31 +0300

    Create a basic flow to add watch later data in DB

---
+++ src/main/java/com/ypm/service/ImportService.java
@@ -0,0 +1,46 @@
+package com.ypm.service;
+
+import com.ypm.persistence.entity.VideoImport;
+import com.ypm.persistence.repository.VideoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ImportService {
+
+    private final VideoRepository videoRepository;
+
+    public List<VideoImport> importCsv(MultipartFile file) throws IOException {
+        final List<VideoImport> videos = new ArrayList<>();
+
+        // Parse CSV
+        try (var reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            String line;
+            var header = true;
+
+            while ((line = reader.readLine()) != null ) {
+                if (header) {
+                    header = false;
+                    continue;
+                }
+
+                var data = line.split(",");
+                var videoId = data[0];
+                var videoTimeStamp = data[1];
+                var video = new VideoImport(videoId, videoTimeStamp);
+                videos.add(video);
+            }
+        }
+
+        // Save into DB
+        return videoRepository.saveAll(videos);
+    }
+}
