76c3088c3a930e5a09b158a5a0ee76cfd2cbe506
Roman Mager <roman.mager@outlook.com>
2024-08-29 10:27:25 +0300

    Implement token caching

--- src/main/java/com/ypm/service/TokenService.java
+++ src/main/java/com/ypm/service/TokenService.java
@@ -6,10 +6,15 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
 public class TokenService {
 
     private final OAuth2AuthorizedClientService authorizedClientService;
+
+    private String cachedToken;
+    private Instant expiresAt;
 
     public TokenService(OAuth2AuthorizedClientService authorizedClientService) {
         this.authorizedClientService = authorizedClientService;
@@ -20,12 +25,23 @@ public class TokenService {
     }
 
     public String getToken() {
-        var authentication = SecurityContextHolder.getContext().getAuthentication();
-        var oauthToken = (OAuth2AuthenticationToken) authentication;
+        if (isTokenExpired()) refreshToken();
+
+        return cachedToken;
+    }
+
+    private boolean isTokenExpired() {
+        return cachedToken == null || Instant.now().isAfter(expiresAt);
+    }
+
+    private void refreshToken() {
+        var oauthToken = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
         var clientRegistrationId = oauthToken.getAuthorizedClientRegistrationId();
         var principalName = oauthToken.getPrincipal().getName();
         var client = authorizedClientService.loadAuthorizedClient(clientRegistrationId, principalName);
 
-        return client.getAccessToken().getTokenValue();
+        var accessToken = client.getAccessToken();
+        cachedToken = accessToken.getTokenValue();
+        expiresAt = accessToken.getExpiresAt();
     }
 }
