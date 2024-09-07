62ac53e479eeed527f2bdc49a7aee758e62f160a
Roman Mager <roman.mager@outlook.com>
2024-09-07 10:23:39 +0300

    Add entities representing DB tables

---
+++ src/main/java/com/ypm/persistence/entity/Video.java
@@ -0,0 +1,45 @@
+package com.ypm.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "videos", schema = "ypm")
+public class Video {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "youtube_id", nullable = false, length = Integer.MAX_VALUE, unique = true)
+    private String youtubeId;
+
+    @Column(name = "import_date")
+    private LocalDate importDate;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "playlist_id", nullable = false)
+    private Playlist playlist;
+
+    @OneToOne(mappedBy = "video", cascade = CascadeType.ALL)
+    private VideoData videoData;
+
+    public Video(String youtubeId, LocalDate importDate) {
+        this.youtubeId = youtubeId;
+        this.importDate = importDate;
+    }
+
+    public Video(String youtubeId, LocalDate importDate, Playlist playlist) {
+        this.youtubeId = youtubeId;
+        this.importDate = importDate;
+        this.playlist = playlist;
+    }
+}
