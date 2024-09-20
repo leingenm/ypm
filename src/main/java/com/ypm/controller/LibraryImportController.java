b0c0601772e85618cdd3adf996a9ff8a64f2dedf
Roman Mager <roman.mager@outlook.com>
2024-09-20 12:08:41 +0300

    Utilize ImportService in LibraryImportController

--- src/main/java/com/ypm/controller/LibraryImportController.java
+++ src/main/java/com/ypm/controller/LibraryImportController.java
@@ -1,14 +1,13 @@
 package com.ypm.controller;
 
-import com.ypm.persistence.entity.VideoImport;
 import com.ypm.service.youtube.ImportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/import")
@@ -18,15 +17,13 @@ public class LibraryImportController {
     private final ImportService importService;
 
     @PostMapping("/watch-later")
-    public ResponseEntity<String> importWatchLaterLibrary(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<String> importWatchLaterLibrary(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
 
-        List<VideoImport> savedVideos;
-        savedVideos = importService.importCsv(file);
+        importService.importVideos(file);
 
-        var responseBody = String.format("Saved %s videos", savedVideos.size());
-        return ResponseEntity.ok().body(responseBody);
+        return ResponseEntity.accepted().body("Videos are being processed. Please check back later.");
     }
 }
