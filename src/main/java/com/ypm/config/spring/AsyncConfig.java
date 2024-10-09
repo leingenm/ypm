860afe5d272f23c1f6d7db2cfca805dedbc2662d
Roman Mager <roman.mager@outlook.com>
2024-10-09 12:02:57 +0300

    Add async configuration for Spring

---
+++ src/main/java/com/ypm/config/spring/AsyncConfig.java
@@ -0,0 +1,31 @@
+package com.ypm.config.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Bean
+    @Override
+    public Executor getAsyncExecutor() {
+        var corePoolSize = Runtime.getRuntime().availableProcessors();
+
+        var executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(corePoolSize * 2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AsyncThread-");
+        executor.initialize();
+
+        // Wrapping with DelegatingSecurityContextExecutor to propagate security context
+        return new DelegatingSecurityContextAsyncTaskExecutor(executor);
+    }
+}
